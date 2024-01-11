0705026863a170aa99ed8dde3cb12a0cc831b54e
BenPalac1 <ben.palacpac@gmail.com>
2024-01-11 13:24:44 -0500

    Set up asset cache in src-sq.js file to filter request

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -26,5 +26,16 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Implement asset caching
+registerRoute(
+  // callback function to filter requests we want cached
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
